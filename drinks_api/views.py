2c01b21ae4dda43bcbaa4f89135ecb49e50e00b5
Krste <toshev.krste@yahoo.com>
2024-06-21 12:27:49 +0200

    Create Drink Serializer. Display Serializer in views. Connect Serializer result to url

--- drinks_api/views.py
+++ drinks_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
 
-# Create your views here.
+
+def drink_list(request):
+    drinks = Drink.objects.all()
+    serializer = DrinkSerializer(drinks, many=True)
+    return JsonResponse({"drinks": serializer.data}, safe=False)
